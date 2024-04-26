938280a765c937e1fae28011043b08ac7da28213
Ryan Peach <rgpeach10@gmail.com>
2024-04-26 14:45:39 -0400

    Added clap cli

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,26 @@
 mod meilisearch;
+use dotenv::dotenv;
+
+use clap::{command, Parser};
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    name: String,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
 
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name)
+    }
 }
