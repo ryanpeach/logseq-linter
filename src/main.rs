59d9b20a18b01261e9db324d38b1f825429ffcd2
Ryan Peach <rgpeach10@gmail.com>
2024-04-27 13:21:05 -0400

    Loading markdown into database, but not currently adapting the ast into our custom format

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,64 @@
 //! The entry point of the program.
 #![warn(missing_docs)]
 #![warn(clippy::missing_docs_in_private_items)]
+#[macro_use]
 
 mod meilisearch;
+mod logseq;
+mod files;
+use std::path::Path;
+
 use dotenv::dotenv;
 
 use clap::{command, Parser};
+use logseq::File;
+use meilisearch_sdk::Client;
+use std::env;
+use tokio;
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref MEILISEARCH_URL: String = 
+        env::var("MEILISEARCH_URL").unwrap_or_else(|_| "http://localhost:7700".to_string());
+    static ref MEILISEARCH_API_KEY: String = 
+        env::var("MEILISEARCH_API_KEY").unwrap_or_else(|_| "masterKey".to_string());
+}
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    name: String,
-
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+    /// Input folder path
+    path: Box<Path>,
 }
 
-fn main() {
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    // Load environment variables from a .env file
     dotenv().ok();
-    let args = Args::parse();
 
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name)
+    // Create a client (without sending any request so that can't fail)
+    let client = Client::new(&*MEILISEARCH_URL, Some(&*MEILISEARCH_API_KEY));
+
+    // An index is where the documents are stored.
+    let logseq = client.index("logseq");
+
+    // Load the files into the database
+    let args = Args::parse();
+    let walker = files::MdWalker::new(args.path.to_str().unwrap());
+    let mut i = 0;
+    for file in walker {
+        let doc = match file {
+            Ok(ast) => File::new(i, ast, args.path.clone()),
+            Err(msg) => {
+                eprintln!("{}", msg);
+                continue;
+            }
+        };
+        logseq
+            .add_documents(&[doc], Some("id"))
+            .await
+            .expect("Cannot add documents");
+        i += 1;
     }
 }
