8335b3d23c0dfeb5c228d9696e5900280d0cd121
Ryan Peach <rgpeach10@gmail.com>
2024-04-28 23:44:36 -0400

    Untested but file seems implemented

--- src/parsing/file.rs
+++ src/parsing/file.rs
@@ -1,6 +1,7 @@
 use std::{collections::HashMap, path::Path};
 
 use markdown::mdast::Node;
+use regex::Regex;
 use serde::{Deserialize, Serialize};
 
 pub struct FileBuilder {
@@ -26,50 +27,138 @@ impl FileBuilder {
         self
     }
 
-    fn get_id(_ast: &Node) -> String {
-        todo!("Get the id from the content");
+    fn get_content(&self) -> Result<String, String> {
+        let file_path = self.path.clone().ok_or("No path".to_string())?;
+        let buf = std::fs::read_to_string(file_path).map_err(|e| e.to_string())?;
+        Ok(buf)
     }
 
-    fn get_properties(_ast: &Node) -> HashMap<String, String> {
-        todo!("Get the properties from the AST")
+    fn get_top_text(ast: &Node) -> String {
+        let top_text = ast
+            .children()
+            .unwrap()
+            .iter()
+            .flat_map(|node| node.children().unwrap())
+            .filter_map(|child| match child {
+                Node::Paragraph(paragraph) => Some(paragraph),
+                _ => None,
+            })
+            .flat_map(|paragraph| paragraph.children.iter())
+            .filter_map(|child| match child {
+                Node::Text(text) => Some(text.value.clone()),
+                _ => None,
+            })
+            .collect();
+        top_text
     }
 
-    fn get_wikilinks(_ast: &Node) -> Vec<String> {
-        todo!("Get the wikilinks from the AST")
+    fn get_id() -> String {
+        uuid::Uuid::new_v4().to_string()
     }
 
-    fn get_tags(_ast: &Node) -> Vec<String> {
-        todo!("Get the tags from the AST")
+    fn get_properties(top_text: &str) -> HashMap<String, String> {
+        let mut properties = HashMap::new();
+        for line in top_text.lines() {
+            let split = line.split("::").map(|s| s.to_string()); // Convert iterator over &str to iterator over String
+            if let [key, value] = split.collect::<Vec<String>>().as_slice() {
+                match key.as_str() {
+                    "title" => {}
+                    "tags" => {}
+                    _ => {
+                        properties.insert(key.clone(), value.clone());
+                    }
+                }
+            }
+        }
+        properties
     }
 
-    fn get_title(_ast: &Node) -> String {
-        todo!("Get the title from the AST")
+    fn get_wikilinks(content: &str) -> Vec<String> {
+        // [[something]] but not #[[something]]
+        let re = Regex::new(r"\s\[\[([\w\s]+)\]\]").unwrap();
+        let mut wikilinks = vec![];
+        for captures in re.captures_iter(content) {
+            assert_eq!(
+                captures.len(),
+                2,
+                "There should be the full capture and the wikilink: {:?}",
+                captures
+            );
+            wikilinks.push(captures[1].trim().to_string());
+        }
+        wikilinks
     }
 
-    pub fn build(mut self) -> File {
-        let ast = self.ast.take().expect("No AST");
-        let path = self
-            .path
-            .take()
-            .expect("No path")
+    fn get_tags(top_text: &str, content: &str) -> Vec<String> {
+        // #something or #[[something]]
+        let re = Regex::new(r"(?i)#\[\[([\w\s]+)\]\]|#(\w+)").unwrap();
+        let mut tags = vec![];
+        for captures in re.captures_iter(content) {
+            assert_eq!(
+                captures.len(),
+                3,
+                "There should be the full capture and the tag: {:?}",
+                captures
+            );
+            if let Some(tag) = captures.get(1) {
+                tags.push(tag.as_str().to_string());
+            } else if let Some(tag) = captures.get(2) {
+                tags.push(tag.as_str().to_string());
+            } else {
+                panic!("No tag found");
+            }
+        }
+        for line in top_text.lines() {
+            let split = line.split("::").map(|s| s.to_string()); // Convert iterator over &str to iterator over String
+            if let [key, value] = split.collect::<Vec<String>>().as_slice() {
+                if key.as_str() == "tags" {
+                    let tags_split: Vec<&str> = value.split(',').collect();
+                    let trim_tags_split: Vec<&str> = tags_split.iter().map(|x| x.trim()).collect();
+                    tags.extend(trim_tags_split.iter().map(|x| x.to_string()));
+                }
+            }
+        }
+        tags
+    }
+
+    fn get_title(path: &Path) -> String {
+        let file_name = path
             .file_name()
             .expect("No file name")
             .to_str()
-            .expect("No file name")
+            .expect("No file name");
+        file_name.replace(".md", "").replace("___", "/")
+    }
+
+    pub fn build(mut self) -> Result<File, String> {
+        let ast = self.ast.take().ok_or("No AST".to_string())?;
+        let path = self
+            .path
+            .clone()
+            .ok_or("No path".to_string())?
+            .to_string_lossy()
             .to_string();
-        let id = Self::get_id(&ast);
-        let properties = Self::get_properties(&ast);
-        let wikilinks = Self::get_wikilinks(&ast);
-        let tags = Self::get_tags(&ast);
-        let title = Self::get_title(&ast);
-        File {
+        let top_text = Self::get_top_text(&ast);
+        let content = self.get_content()?;
+        let id = Self::get_id();
+        let properties = Self::get_properties(&top_text);
+        let wikilinks = Self::get_wikilinks(&content);
+        let tags = Self::get_tags(&top_text, &content);
+        let title = Self::get_title(
+            self.path
+                .take()
+                .ok_or("No path".to_string())
+                .as_ref()
+                .unwrap(),
+        );
+        Ok(File {
             id,
             path,
             title,
             properties,
             wikilinks,
             tags,
-        }
+        })
     }
 }
 
